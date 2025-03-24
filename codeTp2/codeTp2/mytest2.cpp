9e2d5b9d6aa72bc635c59be77038c4ce0940ac2d
AndreLouisSt <stephantandrelouis@gmail.com>
2025-03-24 11:02:35 +0000

    Question 6 ok

--- codeTp2/codeTp2/mytest2.cpp
+++ codeTp2/codeTp2/mytest2.cpp
@@ -5,10 +5,10 @@ TEST(FactorialTest, ZeroValue) {
   EXPECT_EQ(1,factorielle(0));
 }
 
-TEST(FactorialTest, StandardValues) {
+TEST(FactorialTest, DISABLED_StandardValues) {
   EXPECT_EQ(1,factorielle(1));
   ASSERT_EQ(6,factorielle(3));
-  ASSERT_EQ(24,factorielle(4));
+  ASSERT_EQ(24, factorielle(4));
   EXPECT_EQ(120,factorielle(5));
 }
 
